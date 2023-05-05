a84d288caca8fadc79889e5fd0227d1ad5177000
alexkarunny <smetan4ik1@gmail.com>
2023-05-05 13:10:53 +0300

    10_preloader

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,21 @@
+type stateType = {
+    isLoading: boolean
+}
+
+type actionsType = LoadingActionType
+
 const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: stateType = initState, action: actionsType): stateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return {
+                ...state,
+                isLoading: action.isLoading
+            }
         default:
             return state
     }
@@ -15,6 +25,7 @@ type LoadingActionType = {
     type: 'CHANGE_LOADING'
     isLoading: boolean
 }
+
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
